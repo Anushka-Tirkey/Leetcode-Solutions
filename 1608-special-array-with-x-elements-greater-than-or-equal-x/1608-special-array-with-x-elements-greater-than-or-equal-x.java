ea05396720e27e508a425464d9c911032677dfae
Anushka Tirkey <104654032+Anushka-Tirkey@users.noreply.github.com>
2024-05-28 01:52:32 +0530

    Time: 2 ms (47.95%), Space: 41 MB (83.22%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int count=0, k=0;
+        for(int i=0; i<=nums.length; i++)
+        {
+            for(int j=0; j<nums.length; j++)
+            {
+                if(nums[j]>=i)
+                    count++;
+            }
+            if(count==i)
+                return i;
+            count=0;
+        }
+        return -1;
+    }
+}
